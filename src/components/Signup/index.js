9eb4b72df19c05054d3adee7f189f8545eea7dac
UnmarkedBanshee <dxp38@pct.edu>
2023-03-31 20:28:48 -0400

    fixed input handaling on signup page

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -2,8 +2,9 @@ import Signupforms from "./signupuser";
 import './index.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
 
-
 function Signup() {
+    //apply styling to the signup page so that it shrinks and expands with the screen and is responsive
+    //youtube video on web dev simplified for help
     return (
         <div className="register-main">
             <TwitterIcon className="twitter-logo" />
--- src/components/Signup/signupuser/index.js
+++ src/components/Signup/signupuser/index.js
@@ -1,41 +1,41 @@
-//forms and stuff here
-//use this to pass the component for login
 import './index.css';
 import Axios from 'axios';
-import React from "react";
-//this is going to be the frontend stuff
+import { useState, React } from 'react';
+
 function Signupforms() {
-    //create a state for the username and password
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    //test commit
-    
-    //will run through the onClick command in HTML button
-    function register() {
-        Axios.post('http://localhost:3002/login', {username: setUsername, password: setPassword}).then((response) => {
-            console.log(response);
-        });
-    }
+    //once it works change all the usenames to emails
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setUsername('');
+        setPassword('');
+        setMessage(`hello ${username}!`);
+    };
 
     return (
         <div>
             <h2 className="title-signup-page">Join Twitter today</h2>
-            <div className="forms">
+            <form onSubmit={handleSubmit}>
                 <div className='username'>
-                    <input name="username" placeholder="username" className="user-input" type="text" onChange={(e) => {
-                        setUsername(e.target.value);
+                    <input value={username} name="username" placeholder="username" className="user-input" type="text" onChange={(event) => {
+                        setUsername(event.target.value);
                     }}></input>
                 </div>
                 <div className='password'>
-                    <input name="password" placeholder="password" className="pass-input" type="text" onChange={(e) => {
-                        setPassword(e.target.value);
+                    <input value={password} name="password" placeholder="password" className="pass-input" type="text" onChange={(event) => {
+                        setPassword(event.target.value);
                     }}></input>
                 </div>
-                <button className="register-button" onclick={register} >Next</button>
-                <div className='returning-user'>
-                    <p className='returning-user-text'>Already have and account?</p>
-                    <a href='/' className='signup=link'>Sign in</a>
-                </div>
+               <button className="register-button" type='submit'>Next</button>
+               <h2>{message}</h2>
+            </form>
+            <div className='returning-user'>
+                <p className='returning-user-text'>Already have and account?</p>
+                <a href='/' className='signup=link'>Sign in</a>
             </div>
         </div>
     )
