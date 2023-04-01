21a6a42cb37284f5f63b7847e74ca26005db5851
UnmarkedBanshee <dxp38@pct.edu>
2023-03-31 20:03:42 -0400

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom' //always use this for routing react apps
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +12,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-//browser router is than under ReactStrictMode
-//our <App /> is than rapped using BrowserRouter, because the router will effect the whole app
-reportWebVitals();
